0b447f8d3e35b2fd52f132ead4900c5d9d37dc8f
Ayushidwi <38401971+Ayushidwi@users.noreply.github.com>
2019-12-30 23:28:20 +0530

    Add files via upload

---
+++ dp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+const int inf=(int)1e9;
+int findways(int n,int m)
+{
+        int q1=inf, q2=inf, q3=inf;
+
+}
+
+int main()
+{
+
+    int n,m;
+    cin>>n>>m;
+    int c[m];
+    for(int i=0;i<m;i++)
+    {
+
+        cin>c[i];
+    }
+    int ans=findways(n,m);
+}
